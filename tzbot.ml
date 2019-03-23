2292868dfdb253bb6e1a77338f594a9c9ba05fa7
Louis Roché <louis@louisroche.net>
2019-03-23 08:04:45 +0000

    add http server

--- tzbot.ml
+++ tzbot.ml
@@ -17,6 +17,30 @@ let user_of_slack (u : Slacko.user_obj) =
   { name = u.name; tz = u.tz; tz_offset = u.tz_offset; tz_label = u.tz_label }
 
 
+type timezone =
+  { users : users
+  ; label : string option
+  ; zone : Time.Zone.t
+  }
+
+let timezone ~key:tz_offset ~data:users =
+  let hours = tz_offset / (60 * 60) in
+  let zone = Time.Zone.of_utc_offset ~hours in
+  let tz_label = List.find_map users ~f:(fun user -> user.tz_label) in
+  let tz = List.find_map users ~f:(fun user -> user.tz) in
+  let label = Option.first_some tz_label tz in
+  { users; zone; label }
+
+
+let timezones users =
+  let tz_offset_map = Map.empty (module Int) in
+  let tz_offset_map =
+    List.fold_left users ~init:tz_offset_map ~f:(fun map u ->
+        Map.add_multi map ~key:u.tz_offset ~data:u )
+  in
+  Map.mapi tz_offset_map ~f:timezone
+
+
 let filter_active (u : Slacko.user_obj) =
   not (u.is_bot || u.deleted || u.is_ultra_restricted)
 
@@ -77,91 +101,149 @@ let load_users storage =
   content |> Sexp.of_string |> users_of_sexp |> Lwt.return
 
 
-let sexp_display users =
-  users
-  |> List.sort ~compare:(fun a b -> compare a.tz_offset b.tz_offset)
-  |> List.iter ~f:(fun u ->
-         u |> sexp_of_user |> Sexp.to_string_hum |> print_endline )
+module Http = struct
+  open Cohttp_lwt_unix
+
+  type tz =
+    { names : string list
+    ; label : string option
+    ; zone : string
+    }
+  [@@deriving sexp]
+
+  type tzs = tz list [@@deriving sexp]
+
+  let timezone tz =
+    let names = List.map tz.users ~f:(fun u -> u.name) in
+    { names; label = tz.label; zone = Time.Zone.name tz.zone }
 
 
-let tz_display now users =
-  let names = List.map ~f:(fun u -> u.name) users in
-  let names = String.concat ~sep:", " names in
-  let { tz_offset; _ } = List.hd_exn users in
-  let tz_label = List.find_map users ~f:(fun user -> user.tz_label) in
-  let tz = List.find_map users ~f:(fun user -> user.tz) in
-  let hours = tz_offset / (60 * 60) in
-  let zone = Time.Zone.of_utc_offset ~hours in
-  let time = Time.to_string_trimmed now ~zone in
-  let label =
-    match (tz_label, tz) with
-    | Some label, _ | None, Some label ->
-        label
-    | None, None ->
-        "-"
+  let refresh token storage =
+    try%lwt
+      let%lwt () = refresh_users token storage in
+      Server.respond_string ~status:`OK ~body:"done" ()
+    with
+    | exn ->
+        let body =
+          sprintf "unable to refresh users:\n%s" (Exn.to_string exn)
+        in
+        Server.respond_string ~status:`Internal_server_error ~body ()
+
+
+  let tz storage =
+    let%lwt users = load_users storage in
+    let timezones = timezones users in
+    let timezones = Map.map timezones ~f:timezone in
+    Lwt.return (Map.data timezones)
+
+
+  let start port (token, storage) =
+    let callback _conn req _body =
+      let uri = Request.uri req in
+      match Uri.path uri with
+      | "/version" ->
+          Server.respond_string ~status:`OK ~body:"%%VERSION%%" ()
+      | "/refresh" ->
+          refresh token storage
+      | "/sexp/users" ->
+          let%lwt users = load_users storage in
+          let body = users |> sexp_of_users |> Sexp.to_string_hum in
+          Server.respond_string ~status:`OK ~body ()
+      | "/sexp/tz" ->
+          let%lwt timezones = tz storage in
+          let body = timezones |> sexp_of_tzs |> Sexp.to_string_hum in
+          Server.respond_string ~status:`OK ~body ()
+      | _ ->
+          Server.respond_string ~status:`Not_found ~body:"path not found" ()
+    in
+    Lwt_main.run
+      (let%lwt () = Lwt_io.printlf "starting http server on port %d" port in
+       let%lwt () =
+         Server.create ~mode:(`TCP (`Port port)) (Server.make ~callback ())
+       in
+       Lwt.return (`Ok ()))
+end
+
+module Text = struct
+  let sexp_display users =
+    users
+    |> List.sort ~compare:(fun a b -> compare a.tz_offset b.tz_offset)
+    |> List.iter ~f:(fun u ->
+           u |> sexp_of_user |> Sexp.to_string_hum |> print_endline )
+
+
+  let tz_display now { users; label; zone } =
+    let label = Option.value label ~default:"-" in
+    let names = List.map ~f:(fun u -> u.name) users in
+    let names = String.concat ~sep:", " names in
+    let time = Time.to_sec_string now ~zone in
+    printf "%30s | %20s | %s\n" label time names
+
+
+  let table_display users =
+    let timezones = timezones users in
+    let now = Time.now () in
+    Map.iter timezones ~f:(tz_display now)
+
+
+  let display refresh (token, storage) =
+    Lwt_main.run
+      (let%lwt () =
+         if refresh then refresh_users token storage else Lwt.return_unit
+       in
+       let%lwt users = load_users storage in
+       table_display users ;
+       Lwt.return (`Ok ()))
+end
+
+open Cmdliner
+
+let copts token storage = (token, storage)
+
+(* Options common to all commands *)
+let copts_t =
+  let docs = Manpage.s_common_options in
+  let token =
+    let doc = "The Slack API access token" in
+    Arg.(
+      required & pos 0 (some string) None & info [] ~docv:"TOKEN" ~doc ~docs)
   in
-  printf "%30s | %30s | %s\n" label time names
-
-
-let table_display users =
-  let tz_offset_map = Core_kernel.Map.empty (module Int) in
-  let tz_offset_map =
-    List.fold_left users ~init:tz_offset_map ~f:(fun map u ->
-        Map.add_multi map ~key:u.tz_offset ~data:u )
-  in
-  let now = Time.now () in
-  Map.iter tz_offset_map ~f:(tz_display now)
-
-
-let execute token refresh storage =
-  Lwt_main.run
-  @@
   let storage =
-    match storage with
-    | None ->
-        failwith "path to storage file must be provided"
-    | Some storage ->
-        storage
+    let doc = "The file to store users information" in
+    Arg.(
+      value
+      & opt string "tzbot.sexp"
+      & info [ "s"; "storage" ] ~docv:"STORAGE" ~doc ~docs)
   in
-  let%lwt () =
-    if refresh then refresh_users token storage else Lwt.return_unit
-  in
-  let%lwt users = load_users storage in
-  table_display users ;
-  Lwt.return_unit
-
-
-let token =
-  let doc = "The Slack API access token" in
-  Cmdliner.Arg.(
-    required
-    & opt (some string) None
-    & info [ "t"; "token" ] ~docv:"TOKEN" ~doc)
-
-
-let storage =
-  let doc = "The file to store users information" in
-  Cmdliner.Arg.(
-    value
-    & opt (some string) (Some "tzbot.sexp")
-    & info [ "s"; "storage" ] ~docv:"STORAGE" ~doc)
+  Term.(const copts $ token $ storage)
 
 
 let refresh =
   let doc = "Refresh the timezone of users" in
-  Cmdliner.Arg.(value & flag & info [ "r"; "refresh" ] ~docv:"REFRESH" ~doc)
+  Arg.(value & flag & info [ "r"; "refresh" ] ~docv:"REFRESH" ~doc)
 
 
-let info =
-  let doc = "Posts messages to Slack" in
-  Cmdliner.Term.info "tzbot" ~doc
+let http_server_cmd =
+  let doc = "Http server providing timezones as json" in
+  let info = Term.info "server" ~doc in
+  let port =
+    Arg.(value & opt int 8000 & info [ "p"; "port" ] ~docv:"PORT" ~doc)
+  in
+  (Term.(const Http.start $ port $ copts_t), info)
 
 
-let execute_t = Cmdliner.Term.(pure execute $ token $ refresh $ storage)
+let text_table_cmd =
+  let doc = "Display the timezones of slack users in a table" in
+  let info = Term.info "table" ~doc in
+  (Term.(const Text.display $ refresh $ copts_t), info)
+
+
+let default_cmd =
+  let doc = "A simple tool to get the timezones in a slack group" in
+  let exits = Term.default_exits in
+  ( Term.(ret (const (fun _ -> `Help (`Pager, None)) $ copts_t))
+  , Term.info "tzbot" ~doc ~exits )
+
 
 let () =
-  match Cmdliner.Term.eval (execute_t, info) with
-  | `Error _ ->
-      exit 1
-  | _ ->
-      exit 0
+  Term.(exit @@ eval_choice default_cmd [ text_table_cmd; http_server_cmd ])
