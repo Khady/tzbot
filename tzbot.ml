beb6f79e54dc24b2bfab47107fa0c2b170ada0dd
Louis Roché <louis@louisroche.net>
2019-03-24 04:59:53 +0000

    add license

--- tzbot.ml
+++ tzbot.ml
@@ -1,3 +1,24 @@
+(* {{{ COPYING *(
+
+This file is part of tzbot, a program to list timezones in a slack group
+
+Copyright (C) 2019  Louis Roché <louis(_)louisroche.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+)* }}} *)
+
 open Core_kernel
 open Printf
 open Sexplib
